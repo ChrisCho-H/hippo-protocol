dc8563512c8bd1b3f3f6170f9b4355eb56a44799
Chris Hyunhum Cho <c.hyunhum@gmail.com>
2025-03-27 18:05:25 +0900

    Update cli_test.go

--- test/e2e/cli_test.go
+++ test/e2e/cli_test.go
@@ -14,8 +14,8 @@ type Test struct {
 	errorMsg string
 }
 
-const delegator_address = `$(echo -e "password" | go run hippod/main.go keys show alice --keyring-backend file | awk '/address:/ {print $2}')` // Address of the delegator
-const validator_address = `$(echo -e "password" | go run hippod/main.go keys show alice --keyring-backend file | awk '/address:/ {print $2}')` // Address of the validator
+const delegator_address = `$(echo -e "password" | go run hippod/main.go keys show alice --keyring-backend file | awk '/address:/ {print $3}')` // Address of the delegator
+const validator_address = `$(echo -e "password" | go run hippod/main.go keys show alice --keyring-backend file | awk '/address:/ {print $3}')` // Address of the validator
 const target_address = "hippo1mj5e9kpths3x5qsxarax9c50dadumyj8rqxq95"                                                                          // any bech32 hippo address that is tx target(used for sending, ...etc)
 const passphrase = "password"                                                                                                                  // used when sending tx
 
